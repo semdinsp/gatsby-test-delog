6f56b08ef0dea132454b142d1ee900b1ed1901e1
semdinsp <scott.sproule@ficonab.com>
2020-11-21 12:29:35 +0800

    update to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,6 +18,10 @@ const ContactPage = ({
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
           <p>Let our team help you kick start your next project in Timor Leste &rarr;</p>
+    <p>Timor Leste contact: Eka Mardiarti</p>
+    <p>Singapore contact: Scott Sproule</p>
+    <p>Address:  Timor Plaza, CDB 2, 2 Andar, Aldela 20 de Setembro, Bebonuk Dom Aleixo, Dili, Timor Leste </p>
+    
         </div>
         <div>
           <form className="form-container" name="contact-marketing" data-netlify="true"  data-netlify-honeypot="bot-field" action="/success"  method="POST">
